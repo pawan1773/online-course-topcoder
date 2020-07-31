0bd532e875668b78233ee5ae5073237e9e1d5e10
pawan1773 <joginder.pawan@gmail.com>
2020-07-31 19:35:19 +0530

    registration-form

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,11 @@
+/* wait for document to get ready */
+$(document).ready(function() {
+	/* activate materialize side nav in mobile view */
+	$('.sidenav').sidenav();
+	
+	/* to register user */
+	$("#register-form").submit(function(event) {
+		event.preventDefault();
+		registerUser();
+	});
+})
