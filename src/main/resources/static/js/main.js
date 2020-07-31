a369d50fb702a7afbdaca1515772e7a25997e472
pawan1773 <joginder.pawan@gmail.com>
2020-07-31 20:31:55 +0530

    login form added

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,9 +2,23 @@
 $(document).ready(function() {
 	/* activate materialize side nav in mobile view */
 	$('.sidenav').sidenav();
-	
+
+	/* to display registration form */
+	$('.register-link').click(function() {
+		$('#login-form').trigger("reset");
+		$('#registration-form-container').show();
+		$('#login-form-container').hide();
+	});
+
+	/* to display login form */
+	$('.login-link').click(function() {
+		$('#register-form').trigger("reset");
+		$('#login-form-container').show();
+		$('#registration-form-container').hide();
+	});
+
 	/* to register user */
-	$("#register-form").submit(function(event) {
+	$('#register-form').submit(function(event) {
 		event.preventDefault();
 		registerUser();
 	});
