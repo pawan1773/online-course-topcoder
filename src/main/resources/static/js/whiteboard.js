c9e7d9927f825e46140b8f074d871355eb3e76f3
pawan1773 <joginder.pawan@gmail.com>
2020-08-08 15:03:59 +0530

    test

--- src/main/resources/static/js/whiteboard.js
+++ src/main/resources/static/js/whiteboard.js
@@ -1,67 +1,93 @@
 var clearButton = document.getElementById('white-board-eraser');
-    var canvascontainer = document.getElementById('whiteboard-canvas-container');
-    var canvas = document.getElementById('whiteboard-canvas');
-    var context = canvas.getContext('2d');
-    var radius = (document.getElementById('whiteboard-canvas-container').clientWidth + document.getElementById('whiteboard-canvas-container').clientHeight) / 150;
-    var dragging = false;
-    context.mozImageSmoothingEnabled = false;
-    context.imageSmoothingEnabled = false;
+var canvascontainer = document.getElementById('whiteboard-canvas-container');
+var canvas = document.getElementById('whiteboard-canvas');
+var context = canvas.getContext('2d');
+var radius = (document.getElementById('whiteboard-canvas-container').clientWidth + document
+		.getElementById('whiteboard-canvas-container').clientHeight) / 150;
+var dragging = false;
+context.mozImageSmoothingEnabled = false;
+context.imageSmoothingEnabled = false;
 
-    canvas.width = 1280;
-    canvas.height = 720;
-    canvas.style.width = '100%';
-    canvas.style.height = '100%';
+canvas.width = 1280;
+canvas.height = 720;
+canvas.style.width = '100%';
+canvas.style.height = '100%';
 
-    /* CLEAR CANVAS */
-    function clearCanvas() {
-        context.clearRect(0, 0, canvas.width, canvas.height);
-    }
+/* CLEAR CANVAS */
+function clearCanvas() {
+	context.clearRect(0, 0, canvas.width, canvas.height);
+}
 
-    clearButton.addEventListener('click', clearCanvas);
+clearButton.addEventListener('click', clearCanvas);
 
-    function getMousePosition(e) {
-        var mouseX = e.offsetX * canvas.width / canvas.clientWidth | 0;
-        var mouseY = e.offsetY * canvas.height / canvas.clientHeight | 0;
-        return {x: mouseX, y: mouseY};
-    }
+function getMousePosition(e) {
+	var mouseX = e.offsetX * canvas.width / canvas.clientWidth | 0;
+	var mouseY = e.offsetY * canvas.height / canvas.clientHeight | 0;
+	return {
+		x : mouseX,
+		y : mouseY
+	};
+}
 
-    var putPoint = function (e) {
-    	e.preventDefault();
-        e.stopPropagation();
-        if (dragging) {
-            context.lineTo(getMousePosition(e).x, getMousePosition(e).y);
-            context.lineWidth = radius * 2;
-            context.stroke();
-            context.beginPath();
-            context.arc(getMousePosition(e).x, getMousePosition(e).y, radius, 0, Math.PI * 2);
-            context.fill();
-            context.beginPath();
-            context.moveTo(getMousePosition(e).x, getMousePosition(e).y);
-        }
-    };
+var putPoint = function(e) {
+	e.preventDefault();
+	e.stopPropagation();
+	if (dragging) {
+		context.lineTo(getMousePosition(e).x, getMousePosition(e).y);
+		context.lineWidth = radius * 2;
+		context.stroke();
+		context.beginPath();
+		context.arc(getMousePosition(e).x, getMousePosition(e).y, radius, 0,
+				Math.PI * 2);
+		context.fill();
+		context.beginPath();
+		context.moveTo(getMousePosition(e).x, getMousePosition(e).y);
+	}
+};
 
-    var engage = function (e) {
-        dragging = true;
-        putPoint(e);
-    };
-    var disengage = function () {
-        dragging = false;
-        context.beginPath();
-    };
-    
-    canvas.addEventListener("touchmove", function (e) {
-    	  var touch = e.touches[0];
-    	  var mouseEvent = new MouseEvent("mousemove", {
-    	    clientX: touch.clientX,
-    	    clientY: touch.clientY
-    	  });
-    	  canvas.dispatchEvent(mouseEvent);
-    	}, false);
-    
-    canvas.addEventListener('touchstart', engage, false);
-    canvas.addEventListener('touchend', disengage, false);
-   
-    canvas.addEventListener('mousedown', engage);
-    canvas.addEventListener('mousemove', putPoint);
-    canvas.addEventListener('mouseup', disengage);
-    canvas.addEventListener('contextmenu', disengage);
+var engage = function(e) {
+	dragging = true;
+	putPoint(e);
+};
+var disengage = function() {
+	dragging = false;
+	context.beginPath();
+};
+
+canvas.addEventListener("touchstart", function(e) {
+	//mousePos = getTouchPos(canvas, e);
+	var touch = e.touches[0];
+	var mouseEvent = new MouseEvent("mousedown", {
+		clientX : touch.clientX,
+		clientY : touch.clientY
+	});
+	canvas.dispatchEvent(mouseEvent);
+}, false);
+
+canvas.addEventListener("touchmove", function(e) {
+	var touch = e.touches[0];
+	var mouseEvent = new MouseEvent("mousemove", {
+		clientX : touch.clientX,
+		clientY : touch.clientY
+	});
+	canvas.dispatchEvent(mouseEvent);
+}, false);
+
+canvas.addEventListener("touchend", function(e) {
+	var mouseEvent = new MouseEvent("mouseup", {});
+	canvas.dispatchEvent(mouseEvent);
+}, false);
+
+function getTouchPos(canvasDom, touchEvent) {
+	var rect = canvasDom.getBoundingClientRect();
+	return {
+		x : touchEvent.touches[0].clientX - rect.left,
+		y : touchEvent.touches[0].clientY - rect.top
+	};
+}
+
+
+canvas.addEventListener('mousedown', engage);
+canvas.addEventListener('mousemove', putPoint);
+canvas.addEventListener('mouseup', disengage);
+canvas.addEventListener('contextmenu', disengage);
