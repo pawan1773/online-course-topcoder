8666a2d37285483214a3090b3b5f1bd91e6c50dc
pawan1773 <joginder.pawan@gmail.com>
2020-08-08 16:17:18 +0530

    touch

--- src/main/resources/static/js/whiteboard.js
+++ src/main/resources/static/js/whiteboard.js
@@ -1,93 +1,123 @@
-var clearButton = document.getElementById('white-board-eraser');
-var canvascontainer = document.getElementById('whiteboard-canvas-container');
-var canvas = document.getElementById('whiteboard-canvas');
-var context = canvas.getContext('2d');
-var radius = (document.getElementById('whiteboard-canvas-container').clientWidth + document
-		.getElementById('whiteboard-canvas-container').clientHeight) / 150;
-var dragging = false;
-context.mozImageSmoothingEnabled = false;
-context.imageSmoothingEnabled = false;
+(function() {
+	
+	// Get a regular interval for drawing to the screen
+	window.requestAnimFrame = (function (callback) {
+		return window.requestAnimationFrame || 
+					window.webkitRequestAnimationFrame ||
+					window.mozRequestAnimationFrame ||
+					window.oRequestAnimationFrame ||
+					window.msRequestAnimaitonFrame ||
+					function (callback) {
+					 	window.setTimeout(callback, 1000/60);
+					};
+	})();
 
-canvas.width = 1280;
-canvas.height = 720;
-canvas.style.width = '100%';
-canvas.style.height = '100%';
+	// Set up the canvas
+	var canvas = document.getElementById("whiteboard-canvas");
+	var ctx = canvas.getContext("2d");
+	ctx.strokeStyle = "#000";
+	ctx.lineWith = 2;
 
-/* CLEAR CANVAS */
-function clearCanvas() {
-	context.clearRect(0, 0, canvas.width, canvas.height);
-}
+	var clearBtn = document.getElementById("white-board-eraser");
+	clearBtn.addEventListener("click", function (e) {
+		clearCanvas();
+	}, false);
+	
+	// Set up mouse events for drawing
+	var drawing = false;
+	var mousePos = { x:0, y:0 };
+	var lastPos = mousePos;
+	canvas.width = 700;
+	canvas.height = 400;
+	canvas.style.width = '100%';
+	canvas.style.height = '100%';
+	canvas.addEventListener("mousedown", function (e) {
+		drawing = true;
+		lastPos = getMousePos(canvas, e);
+	}, false);
+	canvas.addEventListener("mouseup", function (e) {
+		drawing = false;
+	}, false);
+	canvas.addEventListener("mousemove", function (e) {
+		mousePos = getMousePos(canvas, e);
+	}, false);
 
-clearButton.addEventListener('click', clearCanvas);
+	// Set up touch events for mobile, etc
+	canvas.addEventListener("touchstart", function (e) {
+		mousePos = getTouchPos(canvas, e);
+		var touch = e.touches[0];
+		var mouseEvent = new MouseEvent("mousedown", {
+			clientX: touch.clientX,
+			clientY: touch.clientY
+		});
+		canvas.dispatchEvent(mouseEvent);
+	}, false);
+	canvas.addEventListener("touchend", function (e) {
+		var mouseEvent = new MouseEvent("mouseup", {});
+		canvas.dispatchEvent(mouseEvent);
+	}, false);
+	canvas.addEventListener("touchmove", function (e) {
+		var touch = e.touches[0];
+		var mouseEvent = new MouseEvent("mousemove", {
+			clientX: touch.clientX,
+			clientY: touch.clientY
+		});
+		canvas.dispatchEvent(mouseEvent);
+	}, false);
 
-function getMousePosition(e) {
-	var mouseX = e.offsetX * canvas.width / canvas.clientWidth | 0;
-	var mouseY = e.offsetY * canvas.height / canvas.clientHeight | 0;
-	return {
-		x : mouseX,
-		y : mouseY
-	};
-}
+	// Prevent scrolling when touching the canvas
+	document.body.addEventListener("touchstart", function (e) {
+		if (e.target == canvas) {
+			e.preventDefault();
+		}
+	}, false);
+	document.body.addEventListener("touchend", function (e) {
+		if (e.target == canvas) {
+			e.preventDefault();
+		}
+	}, false);
+	document.body.addEventListener("touchmove", function (e) {
+		if (e.target == canvas) {
+			e.preventDefault();
+		}
+	}, false);
 
-var putPoint = function(e) {
-	e.preventDefault();
-	e.stopPropagation();
-	if (dragging) {
-		context.lineTo(getMousePosition(e).x, getMousePosition(e).y);
-		context.lineWidth = radius * 2;
-		context.stroke();
-		context.beginPath();
-		context.arc(getMousePosition(e).x, getMousePosition(e).y, radius, 0,
-				Math.PI * 2);
-		context.fill();
-		context.beginPath();
-		context.moveTo(getMousePosition(e).x, getMousePosition(e).y);
+	// Get the position of the mouse relative to the canvas
+	function getMousePos(canvasDom, mouseEvent) {
+		var rect = canvasDom.getBoundingClientRect();
+		return {
+			x: mouseEvent.clientX - rect.left,
+			y: mouseEvent.clientY - rect.top
+		};
 	}
-};
 
-var engage = function(e) {
-	dragging = true;
-	putPoint(e);
-};
-var disengage = function() {
-	dragging = false;
-	context.beginPath();
-};
+	// Get the position of a touch relative to the canvas
+	function getTouchPos(canvasDom, touchEvent) {
+		var rect = canvasDom.getBoundingClientRect();
+		return {
+			x: touchEvent.touches[0].clientX - rect.left,
+			y: touchEvent.touches[0].clientY - rect.top
+		};
+	}
 
-canvas.addEventListener("touchstart", function(e) {
-	//mousePos = getTouchPos(canvas, e);
-	var touch = e.touches[0];
-	var mouseEvent = new MouseEvent("mousedown", {
-		clientX : touch.clientX,
-		clientY : touch.clientY
-	});
-	canvas.dispatchEvent(mouseEvent);
-}, false);
+	// Draw to the canvas
+	function renderCanvas() {
+		if (drawing) {
+			ctx.moveTo(lastPos.x, lastPos.y);
+			ctx.lineTo(mousePos.x, mousePos.y);
+			ctx.stroke();
+			lastPos = mousePos;
+		}
+	}
 
-canvas.addEventListener("touchmove", function(e) {
-	var touch = e.touches[0];
-	var mouseEvent = new MouseEvent("mousemove", {
-		clientX : touch.clientX,
-		clientY : touch.clientY
-	});
-	canvas.dispatchEvent(mouseEvent);
-}, false);
+	function clearCanvas() {
+		canvas.width = canvas.width;
+	}
 
-canvas.addEventListener("touchend", function(e) {
-	var mouseEvent = new MouseEvent("mouseup", {});
-	canvas.dispatchEvent(mouseEvent);
-}, false);
+	// Allow for animation
+	(function drawLoop () {
+		requestAnimFrame(drawLoop);
+		renderCanvas();
+	})();
 
-function getTouchPos(canvasDom, touchEvent) {
-	var rect = canvasDom.getBoundingClientRect();
-	return {
-		x : touchEvent.touches[0].clientX - rect.left,
-		y : touchEvent.touches[0].clientY - rect.top
-	};
-}
-
-
-canvas.addEventListener('mousedown', engage);
-canvas.addEventListener('mousemove', putPoint);
-canvas.addEventListener('mouseup', disengage);
-canvas.addEventListener('contextmenu', disengage);
+})();
